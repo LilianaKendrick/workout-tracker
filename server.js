8b231c43a2e6a6b7d528139d2e551fc26cc812ed
liliyy93 <57776892+liliyy93@users.noreply.github.com>
2020-03-28 21:51:13 -0400

    finally working

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const db = require('./models');
-
-const app = express();
-
 app.use(logger('dev'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(express.static('public'));
-app.use(require('./controllers/workout'))
-app.use(require('./controllers/htmlRoutes'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser: true});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    'mongodb://heroku_04pms54m:u5gl634et900nb29t81mj5krel@ds047752.mlab.com:47752/heroku_04pms54m',
+  {
+    useNewUrlParser: true
+  }
+);
+
+// routes
+app.use(require('./routes/api.js'));
+app.use(require('./routes/view.js'));
 
 app.listen(PORT, () => {
-    console.log(`App running on  http://localhost:${PORT}`);
+  console.log(`App running on port ${PORT}!`);
 });
